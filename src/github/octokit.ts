4862342746da877e3b49ee1d045ad3b6ebd81d12
Chirag <chhirag920@gmail.com>
2026-06-06 23:57:56 +0530

    switched to env-based private key

--- src/github/octokit.ts
+++ src/github/octokit.ts
@@ -1,16 +1,13 @@
 import { createAppAuth } from "@octokit/auth-app";
 import { Octokit } from "@octokit/rest";
-import { readFileSync } from "fs";
 import { env } from "../shared/env";
-
-const privateKey = readFileSync(env.GITHUB_PRIVATE_KEY_PATH, "utf8");
 
 export async function getInstallationOctokit(installationId: number) {
   const octokit = new Octokit({
     authStrategy: createAppAuth,
     auth: {
       appId: env.GITHUB_APP_ID,
-      privateKey,
+      privateKey: env.GITHUB_PRIVATE_KEY,
       installationId,
     },
   });
--- src/shared/env.ts
+++ src/shared/env.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 
 export const env = {
   GITHUB_APP_ID: process.env.GITHUB_APP_ID!,
-  GITHUB_PRIVATE_KEY_PATH: process.env.GITHUB_PRIVATE_KEY_PATH!,
+  GITHUB_PRIVATE_KEY: process.env.GITHUB_PRIVATE_KEY!,
   GITHUB_WEBHOOK_SECRET: process.env.GITHUB_WEBHOOK_SECRET!,
   GROQ_API_KEY: process.env.GROQ_API_KEY!,
 };
